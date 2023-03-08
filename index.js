25e008e6c51d7d6ed82ddbecaa8a360fb8185c26
Praveen Kumar <125293304+Praveen0698@users.noreply.github.com>
2023-03-08 09:20:10 +0530

    Holi Wish

---
+++ index.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function() {
+$('button').on('click', function() {
+      $('body').css('background','linear-gradient(to bottom right,red,yellow,orange')
+      $('.two').css('display','none');
+      $('.one').css('visibility','visible');
+      $('.three').css('visibility','visible');
+   
+//  if(window.scrollY >150) {
+//     $('img').slideDown()
+//  }
+})
+})
